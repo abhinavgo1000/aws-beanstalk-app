c06ad5caea29b0d61a7093bd6a46195f8c38169a
Abhinav Goel <abhinavgo1000@gmail.com>
2024-03-19 09:03:55 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AWS Beanstalk React App
+
+Created for React and Python app hosting on AWS Beanstalk
---
+++ src/App.js
@@ -0,0 +1,8 @@
+import Dogs from './components/dogs'
+import './App.css';
+
+function App() {
+  return <Dogs/>
+}
+
+export default App;
